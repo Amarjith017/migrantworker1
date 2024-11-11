bdbd3a3e5d2a6da5b9f72920c23b00b783cee4d5
Amarjith017 <amarjith017backup@gmail.com>
2024-11-11 18:14:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:migrantworker/worker/screens/login_screen.dart';
+import 'package:migrantworker/worker/screens/regstration.dart';
+
+void main() {
+  runApp(MaterialApp(home: SignUp()));
+}
